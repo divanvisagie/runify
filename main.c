9f4a8da3287df3b95d4c559cf6fc4ebf76ff685a
Divan Visagie <me@divanv.com>
2024-06-07 19:15:25 +0200

    Print help and add flags

--- main.c
+++ main.c
@@ -4,40 +4,84 @@
 
 #include "default.h"
 
+char* get_help() {
+  char* help = "Usage: runify [OPTION]... [STRING]\n"
+               "Transliterate a string from Latin characters to Elder Futhark characters.\n"
+               "\n"
+               "Options:\n"
+               "  -h, --help              Display this help and exit\n"
+               "  -l, --list              List rune systems\n"
+               "  -s, --system=SYSTEM\n   Specify rune system to use\n"
+               "\n"
+               "Examples:\n"
+               "  runify hello world\n"
+               "  runify -h\n";
+  return help;
+}
+
+char* list_rune_systems() {
+  char* systems = "elder:   Elder Futhark\n"
+                  "younger: Younger Futhark\n";
+  return systems;
+}
+
 int main(int argc, char **argv) {
-  // read string piped in from stdin
-  char *input = "helloworld";
+  // if arg is for help
+  if (argc == 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)) {
+    char *help = get_help();
+    printf("%s\n", help);
+    return 0;
+  }
+
+  //if list
+  if (argc == 2 && (strcmp(argv[1], "-l") == 0 || strcmp(argv[1], "--list") == 0)) {
+    char *systems = list_rune_systems();
+    printf("%s\n", systems);
+    return 0;
+  }
+
+  //get the system flag value if it exists or set the default
+  char *system = "elder";
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--system") == 0) {
+      system = argv[i + 1];
+    }
+  }
+
+  char *input = "No input was provided";
   size_t len = 0;
   ssize_t read;
 
+  char all_input[1000];
   // TODO: if you for example pipe ls in, this will only get the first item
   // so we need some better input handling here
-  read = getline(&input, &len, stdin);
-  if (read == -1) {
-    fprintf(stderr, "Error reading input\n");
-    return 1;
+  // Read lines from stdin until EOF
+  while ((read = getline(&input, &len, stdin)) != -1) {
+    size_t input_len = strlen(input);
+    strncat(all_input, input, input_len);
   }
 
-  printf("raw input was: '%s'\n", input);
-  // TODO: Update algorithm to handle newlines and other unexpected chars
-  input[strcspn(input, "\n")] = 0; // Trim newlines from input
+  // trim last newline from input
+  input[strcspn(input, "\n")] = 0;
 
-  printf("trimmed input was: '%s'\n", input);
-
-  // create a new TokenMapper
   TokenMapper *mapper = token_mapper_elder_new();
+  if (strcmp(system, "younger") == 0) {
+    mapper = token_mapper_younger_new();
+  }
+
   if (!mapper) {
     fprintf(stderr, "Error creating TokenMapper\n");
     return 1;
   }
-  //
-  // transliterate the input string
-  printf("Going to transliterate\n");
-  char *output = to_fut(mapper, input);
+  char *output = to_fut(mapper, all_input);
   if (!output) {
     fprintf(stderr, "Error transliterating input\n");
     return 1;
   }
-  // print the output string
   printf("%s\n", output);
+
+  free(output);
+  free(mapper);
+
 }
+
