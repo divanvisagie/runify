4ab8cf6ef148826b2eeea1c50f83e22021289f89
Divan Visagie <me@divanv.com>
2024-06-07 17:58:45 +0200

    Add notes

--- main.c
+++ main.c
@@ -10,13 +10,19 @@ int main(int argc, char **argv) {
   size_t len = 0;
   ssize_t read;
 
-  // read = getline(&input, &len, stdin);
-  // if (read == -1) {
-  //   fprintf(stderr, "Error reading input\n");
-  //   return 1;
-  // }
+  // TODO: if you for example pipe ls in, this will only get the first item
+  // so we need some better input handling here
+  read = getline(&input, &len, stdin);
+  if (read == -1) {
+    fprintf(stderr, "Error reading input\n");
+    return 1;
+  }
 
-  printf("input was: %s\n", input);
+  printf("raw input was: '%s'\n", input);
+  // TODO: Update algorithm to handle newlines and other unexpected chars
+  input[strcspn(input, "\n")] = 0; // Trim newlines from input
+
+  printf("trimmed input was: '%s'\n", input);
 
   // create a new TokenMapper
   TokenMapper *mapper = token_mapper_new();
